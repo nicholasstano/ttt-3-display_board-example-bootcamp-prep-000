3f84d16d66d535393364712498d0c593bc8d3d0d
Nicholas Stano <nicholasstano@gmail.com>
2019-01-25 01:45:34 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,4 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 
 def board_display
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
-puts "-----------"
-puts "   |   |   "
 end
